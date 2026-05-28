978ff2aa090af5a87fba6b5cb4e5a8c45134c98a
marwanlaboudy <158835850+marwanlaboudy@users.noreply.github.com>
2026-05-28 04:39:20 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -262,7 +262,7 @@ def main():
 
     with sync_playwright() as pw:
         context = pw.chromium.launch_persistent_context(
-            user_data_dir="fb_profile",
+            user_data_dir="fb_profile_final",
             headless=False,
             args=BROWSER_ARGS,
             no_viewport=True,
